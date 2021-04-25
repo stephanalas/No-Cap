e255ea5c05754b7b69bd27e624f961c5afe54eea
Stephan Alas <stephan.j.alas@gmail.com>
2021-04-25 18:20:39 -0400

    added associations and testspec

---
+++ server/db/models/associations.js
@@ -0,0 +1,32 @@
+const Order = require('./Order');
+const OrderLineItem = require('./OrderLineItem');
+const { User } = require('./User');
+const { Product } = require('./Product');
+const Review = require('./Review');
+// const Cart = require('./Cart');
+// const CartLineItem = require('./CartLineItem');
+
+// Product
+
+Product.hasMany(Review);
+
+// Order
+
+Order.belongsTo(User);
+Order.hasMany(OrderLineItem);
+OrderLineItem.belongsTo(Order);
+OrderLineItem.belongsTo(Product);
+
+// Cart
+// Cart.belongsTo(User)
+// Cart.hasMany(CartLineItem)
+
+// User
+User.hasMany(Order);
+
+// Review
+Review.belongsTo(User);
+
+module.exports = {
+  models: { Order, OrderLineItem, User, Product, Review },
+};
--- tests/server/db/orderLineItemModel.spec.js
+++ tests/server/db/orderLineItemModel.spec.js
@@ -1,22 +1,22 @@
 /* eslint no-undef: 'off' */
 /* eslint no-console: 'off' */
 
-
 const { db, initDB } = require('../../../server/db/index');
-const OrderLineItem  = require('../../../server/db/models/OrderLineItem');
+const OrderLineItem = require('../../../server/db/models/OrderLineItem');
 
-beforeAll(async()=>{
-    await initDB();
+beforeAll(async () => {
+  await initDB();
 });
 
-afterAll(async()=>{
-    db.close();
+afterAll(async () => {
+  db.close();
 });
 
-it('Order Line Item model exists', async()=>{
-    const line_item = await OrderLineItem.create({
-        unitPrice: 40.5,
-        quantity: 3,
-    });
-    expect(line_item.totalPrice).toBe(121.5);
-})
+it('Order Line Item model exists', async () => {
+  const line_item = await OrderLineItem.create({
+    unitPrice: 40.5000001,
+    quantity: 3,
+  });
+  console.log(line_item.unitPrice);
+  expect(line_item.totalPrice).toBeCloseTo(121.5);
+});
