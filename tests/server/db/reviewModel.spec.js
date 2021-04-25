a04c93cb684452402b77530704b410082becc96e
Sam Fedenia <sam@fedenia.xyz>
2021-04-25 16:24:37 -0500

    added review model and test

---
+++ tests/server/db/reviewModel.spec.js
@@ -0,0 +1,21 @@
+/* eslint no-undef: 'off' */
+/* eslint no-console: 'off' */
+const Review = require('../../../server/db/models/Review');
+const { db, initDB } = require('../../../server/db/index');
+
+let review;
+
+beforeAll(async () => {
+  await initDB();
+});
+
+afterAll(() => {
+  db.close();
+});
+
+it('Review model exists', async () => {
+  review = await Review.create({
+    stars: 4,
+  });
+  expect(review.stars).toBe(4);
+});
