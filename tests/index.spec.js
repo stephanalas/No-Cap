77a72bb1df194e32d6df3cbf30e3250ee2da5bf4
Damien Outar <damienoutar@gmail.com>
2021-04-25 10:28:32 -0400

    added jest travisyml and test specs

---
+++ tests/index.spec.js
@@ -0,0 +1,9 @@
+
+
+it('adds 1 + 2 to equal 3', () => {
+    expect((1)).toBe(1);
+  });
+
+  it('adds 1 + 2 to equal 3', () => {
+    expect((2)).toBe(2);
+  });
---
+++ tests/new.spec.js
@@ -0,0 +1,8 @@
+
+it('adds 1 + 2 to equal 3', () => {
+    expect((5)).toBe(5);
+  });
+
+  it('adds 1 + 2 to equal 3', () => {
+    expect((2)).toBe(2);
+  });
