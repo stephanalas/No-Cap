dc73fb6832c75b805804dfd53b9d4c4687db387b
Anna Litovskaya <Litovskaya09@gmail.com>
2021-04-28 18:48:38 -0400

    sync and seed added

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -12,14 +12,17 @@ class Login extends React.Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
   onChange(ev) {
     this.setState({ [ev.target.name]: ev.target.value });
   }
+
   onSubmit(ev) {
     ev.preventDefault();
     const { email, password } = this.state;
     // thunk needed for submit
   }
+
   render() {
     const { onChange, onSubmit } = this;
     const { email, password } = this.state;
@@ -29,7 +32,7 @@ class Login extends React.Component {
         <input value={email} onChange={onChange} name="username" />
         <label htmlFor="password">Password</label>
         <input value={password} onChange={onChange} name="password" />
-        <button>Login In</button>
+        <button type="submit">Login In</button>
       </form>
     );
   }
