eb7eff89e79f9306e7040a571a711fb840c3d97e
Stephan Alas <stephan.j.alas@gmail.com>
2021-04-28 22:02:08 -0400

    fixed seed and started authentication

--- server/api/loginRouter.js
+++ server/api/loginRouter.js
@@ -4,10 +4,11 @@ const {
   models: { User },
 } = require('../db/models/associations');
 
-const passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-  function(email, password, done) {
+passport.use(
+  new LocalStrategy(function (email, password, done) {
     User.findOne({ email: email }, function (err, user) {
       // if (err) { return done(err); }
       // if (!user) {
@@ -17,28 +18,37 @@ passport.use(new LocalStrategy(
       //   return done(null, false, { message: 'Incorrect password.' });
       // }
       // return done(null, user);
-      if (err) { return done(err); }
-      if (!user) { return done(null, false, { message: 'Incorrect email.' }); }
-      if (!bcrypt.compare(user.password, password)) { return cb(null, false, { message: 'Incorrect password.' }); }
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      if (!bcrypt.compare(user.password, password)) {
+        return cb(null, false, { message: 'Incorrect password.' });
+      }
       return cb(null, user);
     });
-  }
-));
-
+  })
+);
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', passport.authenticate('local'), {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}, 
-), async (req, res, next) => {
-  try {
-    console.log(passport);
-  } catch (error) {
-    next(error);
-  }
-});
+// loginRouter.post(
+//   '/',
+//   passport.authenticate('local'),
+//   {
+//     successRedirect: '/',
+//     failureRedirect: '/login',
+//     failureFlash: true,
+//   },
+//   async (req, res, next) => {
+//     try {
+//       console.log(passport);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
 module.exports = loginRouter;
