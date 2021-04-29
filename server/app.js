5a86fcf480b50f2f0bf2f94853043152fc952af1
Stephan Alas <stephan.j.alas@gmail.com>
2021-04-28 21:51:50 -0400

    beginning of authentication

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const router = require('./api/router');
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.get('/', (req, res, next) => {
