08d8eb9b27087aa366323690225ea711e8b544b1
Stephan Alas <stephan.j.alas@gmail.com>
2021-05-04 14:34:44 -0400

    feature added filtering

--- client/components/Filter.jsx
+++ client/components/Filter.jsx
@@ -13,81 +13,81 @@ class Filter extends React.Component {
         <ul className="category-list">
           <label>Category</label>
           <li>
-            {' '}
-            Beanie
+            {'Beanie'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Beanie"
             ></input>
           </li>
           <li>
-            {' '}
-            Beret
+            {'Beret'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Beret"
             ></input>
           </li>
           <li>
-            {' '}
-            Baseball Hat
+            {'Baseball Hat'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Baseball Hat"
             ></input>
           </li>
           <li>
-            {' '}
-            Fedora
+            {'Fedora'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Fedora"
             ></input>
           </li>
           <li>
-            {' '}
-            Cowboy Hat
+            {'Cowboy Hat'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Cowboy Hat"
             ></input>
           </li>
           <li>
-            {' '}
-            Fez
+            {'Fez'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Fez"
             ></input>
           </li>
           <li>
-            {' '}
-            Top Hat
+            {'Top Hat'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Top Hat"
             ></input>
           </li>
           <li>
-            {' '}
-            other
+            {'Other'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox category"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Other"
             ></input>
@@ -96,63 +96,149 @@ class Filter extends React.Component {
         <ul className="color-list">
           <label htmlFor="">Color</label>
           <li>
-            {' '}
-            Black
+            {'Black'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox color"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Black"
             ></input>
           </li>
           <li>
-            {' '}
-            Brown
+            {'Brown'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox color"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Brown"
             ></input>
           </li>
           <li>
-            {' '}
-            Red
+            {'Red'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox color"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Red"
             ></input>
           </li>
           <li>
-            {' '}
-            Green
+            {'Green'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox color"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Green"
             ></input>
           </li>
           <li>
-            {' '}
-            Blue
+            {'Blue'}
             <input
-              className="filter-checkbox"
+              className="filter-checkbox color"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
               name="Blue"
             ></input>
           </li>
           <li>
-            {' '}
-            Other
+            {'Other'}
+            <input
+              className="filter-checkbox color"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="Other-color"
+            ></input>
+          </li>
+        </ul>
+        <ul className="price-list">
+          <label htmlFor="">Price</label>
+          <li>
+            {'Under $15'}
             <input
               className="filter-checkbox"
               type="checkbox"
+              defaultChecked={false}
               onChange={onChange}
-              name="Other"
+              name="0, 15"
+            ></input>
+          </li>
+          <li>
+            {'$15 to $50'}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="15, 50"
+            ></input>
+          </li>
+          <li>
+            {'$50 & Above'}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="50, null"
+            ></input>
+          </li>
+        </ul>
+        <ul className="ratings">
+          <label htmlFor="">Rating</label>
+          <li>
+            {'5 Stars'}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="5"
+            ></input>
+          </li>
+          <li>
+            {'4 Stars & Above '}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="4"
+            ></input>
+          </li>
+          <li>
+            {'3 Stars & Above '}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="3"
+            ></input>
+          </li>
+          <li>
+            {'2 Stars & Above '}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name="2"
+            ></input>
+          </li>
+          <li>
+            {'1 star & Below '}
+            <input
+              className="filter-checkbox"
+              type="checkbox"
+              defaultChecked={false}
+              onChange={onChange}
+              name={'1&Below'}
             ></input>
           </li>
         </ul>
