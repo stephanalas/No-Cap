7116a632c98e45a7921a804df6e7a66c5b12962e
Damien Outar <damienoutar@gmail.com>
2021-05-07 21:58:58 -0400

    styled previous orders

--- client/components/OrderCard.jsx
+++ client/components/OrderCard.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import './styles/OrderCard.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { StyledTableCell } from './utils/styledTableCell';
 
 class OrderCard extends React.Component {
   constructor(props) {
@@ -10,24 +16,56 @@ class OrderCard extends React.Component {
     console.log('in render', this.props);
     return (
       <div>
-        <div>Current Order Status:{this.props.order.status}</div>
-        <div>Items Ordered:{this.props.order.order_line_items.length}</div>
-        {this.props.order.order_line_items.map((item) => {
-          return (
-            <div key={item.name}>
-              <img src={item.photo} alt={item.name} width='500' height='500' />
-              <div>Product Name:{item.name}</div>
-              <div>Product Unit Price:{item.unitPrice}</div>
-              <div>Quantity Ordered:{item.quantity}</div>
-              <div>Subtotal:{item.subTotal}</div>
-              <div>
-                <a href={`/#/Products/${item.productId}`}>Product Link</a>
-              </div>
-            </div>
-          );
-        })}
-        <div>Order Total:${this.props.order.total}</div>
-        <div>Order Date:{this.props.order.createdAt}</div>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              Current Order Status:{this.props.order.status}
+            </TableHead>
+            <TableHead>
+              Items Ordered:{this.props.order.order_line_items.length}
+            </TableHead>
+            {this.props.order.order_line_items.map((item) => {
+              return (
+                <TableRow key={item.name}>
+                  <StyledTableCell>
+                    <img
+                      src={item.photo}
+                      alt={item.name}
+                      width='500'
+                      height='500'
+                    />
+                  </StyledTableCell>
+                  <StyledTableCell>Product Name:{item.name}</StyledTableCell>
+                  <StyledTableCell>
+                    Product Unit Price:{item.unitPrice}
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    Quantity Ordered:{item.quantity}
+                  </StyledTableCell>
+                  <StyledTableCell>Subtotal:{item.subTotal}</StyledTableCell>
+                  <StyledTableCell>
+                    <a href={`/#/Products/${item.productId}`}>Product Link</a>
+                  </StyledTableCell>
+                </TableRow>
+              );
+            })}
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>
+                  Order Total:${this.props.order.total}
+                </StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>
+                  Order Date:{this.props.order.createdAt}{' '}
+                </StyledTableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+        <hr></hr>
       </div>
     );
   }
